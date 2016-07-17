147c3d4192d03067d59184927073fb1f943a5442
Benjamin <benj@zagorski.com>
2016-07-17 14:43:13 -0700

    Back to draw_first_quarter, as it includes diagonally-reflected points

--- draw_first_eighth.js
+++
@@ -1,7 +0,0 @@
-function draw_first_eighth(){
-  for (var x = 0; x < this.radius/Math.sqrt(2); x++) {
-    // figure out X for this Y
-    draw_point(x, f(x));
-    // Draw that x,y
-  }
-}
---
+++ draw_first_quarter.js
@@ -0,0 +1,15 @@
+function draw_first_quarter(){
+  var point_storage = []
+  // diagonal_eighth is the x-coordinate of the point an eighth
+  // of the way around the circle.
+  var diagonal_eighth = this.radius/Math.sqrt(2) + 1;
+  for (var x = 0; x < diagonal_eighth; x++) {
+    var y = f(x)
+    // This draws the first eighth (45 to 90)
+    draw_point(x, y);
+    point_storage.push([x,y]);
+    // This draws the reflection over the diagonal (0 to 45)
+    draw_point(y, x);
+    point_storage.push([y,x]);
+  }
+}
