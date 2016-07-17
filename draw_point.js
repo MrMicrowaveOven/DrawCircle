89bd823238b12c2703c0bed7a63f51ef7d1048f4
Benjamin <benj@zagorski.com>
2016-07-17 13:16:19 -0700

    draw_point() now takes cartesian coordinates

---
+++ draw_point.js
@@ -0,0 +1,11 @@
+function draw_point(x, y) {
+
+  var coordinates = carte_to_canvas(x,y);
+
+  this.ctx.beginPath();
+
+  this.ctx.rect(coordinates[0], coordinates[1], 1, 1);
+  // this.ctx.fillStyle = "black";
+  this.ctx.fill();
+  this.ctx.closePath();
+}
