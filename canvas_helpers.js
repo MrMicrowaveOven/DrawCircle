189f8416b999721f82faac26c6be0025d65d5d92
Benjamin <benj@zagorski.com>
2016-12-09 20:21:14 -0800

    Added border to canvas

--- canvas_helpers.js
+++ canvas_helpers.js
@@ -16,5 +16,15 @@ function loadCanvas() {
   ctx.fillText("-100",186,255);
   ctx.fillText("100",305,144);
   ctx.fillText("-100",305,344);
+  draw_border()
+}
 
+function draw_border() {
+  this.ctx.beginPath();
+  this.ctx.rect(0,   0,   720, 1  );
+  this.ctx.rect(0,   0,   1,   480);
+  this.ctx.rect(719, 0,   1,   479);
+  this.ctx.rect(0,   479, 720, 1  );
+  this.ctx.fill();
+  this.ctx.closePath();
 }
