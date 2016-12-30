1131efd4c595c22198a04fd4d153e2d1e14e1358
Benjamin <benj@zagorski.com>
2016-12-29 17:01:43 -0800

    Deleted extraneous draw_circle folder

--- draw_circle/getXCoordinate.js
+++
@@ -1,8 +0,0 @@
-// This function returns the y coordinate for a given
-// x coordinate on the circle.
-
-function getXCoordinate(y, radius){
-  var radiusSquared = radius * radius;
-  var ySquared = y * y;
-  return Math.sqrt(radiusSquared - ySquared);
-}
